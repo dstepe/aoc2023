4a4eaeee53787b8f0b044b88f47bdbb82125e360
Dirk Tepe <tepeds@miamioh.edu>
2023-12-09 21:16:32 -0500

    Day 8 part 2 refactored

--- app/Aoc/Day08/Navigator.php
+++ app/Aoc/Day08/Navigator.php
@@ -39,6 +39,59 @@ class Navigator
 
     public function steps(): int
     {
+        $steps = $this->startNodes()->reduce(function (Collection $c, string $label) {
+            $c->add($this->stepsForNode($label));
+            return $c;
+        }, new Collection());
+
+        return $this->findLcm($steps->toArray());
+    }
+
+    private function gcd(int $a, int $b): int
+    {
+        if ($b === 0) {
+            return $a;
+        }
+
+        return $this->gcd($b, $a % $b);
+    }
+
+    private function findLcm(array $arr): int
+    {
+        $n = count($arr);
+        $ans = $arr[0];
+
+        for ($i = 1; $i < $n; $i++)
+            $ans = ((($arr[$i] * $ans)) /
+                ($this->gcd($arr[$i], $ans)));
+
+        return $ans;
+    }
+
+    private function stepsForNode(string $current): int
+    {
+        $count = 0;
+        foreach ($this->instructions->steps() as $direction) {
+            $count++;
+
+            /** @var Node $node */
+            $node = $this->nodes->get($current);
+
+            /** @var Node $next */
+            $next = $this->nodes->get($node->nodeForDirection($direction));
+
+            if (preg_match('/.{2}Z$/', $next->label())) {
+                return $count;
+            }
+
+            $current = $next->label();
+        }
+
+        throw new \InvalidArgumentException('Could not find steps');
+    }
+
+    public function steps2(): int
+    {
         $current = $this->startNodes();
 
         $count = 0;
